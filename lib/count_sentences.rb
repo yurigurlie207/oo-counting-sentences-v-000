2ca07b046337e3ce033c7fdaf9b3f0cf3cc83155
Adrienne Yao <adrienne.yao@icloud.com>
2018-01-25 23:11:25 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
+      stnArray = self.split(/[.!?]/)
+      stnArray.delete("")
+      stnArray.delete(" ")
 
+      return stnArray.length
   end
 end
